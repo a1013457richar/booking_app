2016bc3976ea32b800f1a7f3b9dddf79c6df4514
a1013457richar <r11722013@g.ntu.edu.tw>
2024-02-06 16:11:47 +0800

    change naviagte

--- frontend/src/pages/AddHotels.tsx
+++ frontend/src/pages/AddHotels.tsx
@@ -2,8 +2,10 @@ import { useMutation } from "react-query";
 import ManageHotelsForm from "../form/ManageHotelForm/ManageHotelsForm";
 import { useAppContext } from "../contexts/AppContext";
 import * as apiClient from "../api-client";
+import { useNavigate } from "react-router-dom";
 
 const AddHotels = () => {
+  const navigate = useNavigate();
   const { showToast } = useAppContext();
   const { mutate, isLoading } = useMutation(apiClient.addHotel, {
     onSuccess: () => {
@@ -11,6 +13,7 @@ const AddHotels = () => {
         message: "Hotel added successfully",
         type: "SUCCESS",
       });
+      navigate("/my-hotels");
     },
     onError: () => {
       showToast({
